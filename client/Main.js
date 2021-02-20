f7702f197dadf0f712553bd64334fa293d96ae95
Vasyl Semak <vasylsmk1401@gmail.com>
2021-02-19 20:20:39 -0500

    refactor Albums component to functional, mooved state to Main component

--- client/Main.js
+++ client/Main.js
@@ -4,11 +4,43 @@ import Player from './components/Player'
 import Albums from './components/Albums'
 
 export default class Main extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      albums: [
+        {
+          "id": 1,
+          "name": "No Dummy",
+          "artworkUrl": "default-album.jpg",
+          "artistId": 1,
+          "artist": {
+            "id": 1,
+            "name": "The Crash Test Dummies"
+          }
+        },
+        {
+          "id": 2,
+          "name": "I React to State",
+          "artworkUrl": "default-album.jpg",
+          "artistId": 1,
+          "artist": {
+            "id": 1,
+            "name": "The Crash Test Dummies"
+          }
+        }
+      ]
+    }
+  }
+
+
   render () {
+
     return (
       <div id='main' className='row container'>
         <Sidebar />
-        <Albums />
+        <div class='container'>
+          <Albums albums={this.state.albums} />
+        </div>
         <Player />
       </div>
     )
